4622c0d74f3ee5ecee636ee4927338b3c8a55537
lambda-cat <chen.yack@gmail.com>
2015-03-25 17:45:59 +0800

    ok

--- src/treebear_probe.erl
+++ src/treebear_probe.erl
@@ -1,6 +1,6 @@
 -module(treebear_probe).
--export([start/6]).
-%-compile(export_all).
+%-export([start/6]).
+-compile(export_all).
 %-compile(debug_info).
 
 
@@ -18,10 +18,23 @@ buildPacket(PackCount) ->
 
 buildPacketHeader(PackCount) ->
     ProbeMac = randomMac(),
-    SubPacketLength = 16#0014,
-    Len = SubPacketLength * PackCount + 22,
-    Mode = random:uniform(2) - 1,
-    list_to_binary([<<16#efd1:16,16#fe,Mode,16#0001:16,Len:16,1001:16,1:16>>,ProbeMac,<<PackCount:16,SubPacketLength:16>>]).
+    SubPacketLength = 16#0018,
+    Len = SubPacketLength * PackCount + 30,
+    Mode = 0, %%random:uniform(2) - 1,
+    Sn = randomSn(),
+    list_to_binary(
+      [<<16#efd1:16, %% magic
+	 16#fe, %% code
+	 Mode, %% subCode
+	 16#0001:16, %% version
+	 Len:16, %% length
+	 1001:16, %% venderId
+	 1:16>>, %% productId 
+       Sn, %% sn
+       ProbeMac, %% probeMac
+       <<PackCount:16, %% subPacketLength
+	 SubPacketLength:16>> %% subPacketList
+      ]).
 
 %% build subPacket List , has count of PackCount subPacket.
 buildSubPacket(0,Binary) -> Binary;
@@ -29,18 +42,22 @@ buildSubPacket(PackCount, Binary) ->
     Flag = random:uniform(3),
     DevMac = randomMac(),
     Timestamp = timestampAsLong(),
-    Rssi = 16#01,
+    Rssi = 16#e1,
     Channel = 16#02,
     IsAssociated = 16#01,
-    FrameControl = 16#0001,
+    FrameControl = random:uniform(1000),
     ProbeTimes = random:uniform(255),
-    SubPacket = list_to_binary([<<Flag:16>>,DevMac,<<Timestamp:32,Rssi,Channel,IsAssociated,0,FrameControl:16,ProbeTimes:16>>]),
+    SubPacket = list_to_binary([DevMac,<<Flag:16>>,<<Timestamp:64,Rssi,Channel,IsAssociated,0,FrameControl:16,ProbeTimes:16>>]),
     buildSubPacket(PackCount - 1, [SubPacket|Binary]).
 
 randomMac() ->
     L = randomByteList(6, []),
     list_to_binary(L).
-    
+
+randomSn() -> 
+    I = random:uniform(10),
+    "p_00000" ++ integer_to_list(I - 1).
+
 randomByteList(0, Cps) -> Cps;
 randomByteList(Size, Cps) ->
     R = random:uniform(255),
@@ -49,5 +66,5 @@ randomByteList(Size, Cps) ->
     
 timestampAsLong() ->
     {A,B,C} = os:timestamp(),
-    round(A * math:pow(10,6) + B * math:pow(10,3) + C).
+    round(A * math:pow(10,12) + B * math:pow(10,6) + C).
     
